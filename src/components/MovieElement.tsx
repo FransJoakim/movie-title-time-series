e542fcb6509b450bb2b3e264221889cd81212f2f
Frans J Titulaer <fransjlt@gmail.com>
2022-12-20 23:10:45 +0100

    switching side that modal renders when element rect is outside viewport

--- src/components/MovieElement.tsx
+++ src/components/MovieElement.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import useWindowDimensions from "../utils/useWindowDimensions";
 
 export const MovieElement = ({ Title, Poster }: Movie) => {
+  const { screenWidth } = useWindowDimensions();
   const ref = useRef<HTMLDivElement>(null);
+  const [inViewPort, setInViewPort] = useState(true);
 
   useEffect(() => {
     if (ref.current) ref.current.style.visibility = "hidden";
@@ -12,6 +15,16 @@ export const MovieElement = ({ Title, Poster }: Movie) => {
       <div
         className="movieElement"
         onMouseEnter={() => {
+          if (ref.current) {
+            console.log();
+            console.log(ref.current.getBoundingClientRect());
+          }
+          if (ref.current) {
+            const position = ref.current.getBoundingClientRect();
+            if (screenWidth - (position.x + position.width) < 0) {
+              setInViewPort(false);
+            }
+          }
           if (ref.current) ref.current.style.visibility = "visible";
         }}
         onMouseLeave={() => {
@@ -26,7 +39,7 @@ export const MovieElement = ({ Title, Poster }: Movie) => {
         onMouseLeave={() => {
           if (ref.current) ref.current.style.visibility = "hidden";
         }}
-        className="infoModal"
+        className={`infoModal${inViewPort ? "" : " leftModal"}`}
       >
         <p>{Title}</p>
         <img src={Poster} alt="Poster" />
